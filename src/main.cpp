10a3628a677f72bb92e3c9136b0ee4d2d0bf4563
Ahmed MJADI <92577300+mjadiahmed@users.noreply.github.com>
2024-12-10 14:42:52 +0100

    first commit: works 32bit signal

---
+++ src/main.cpp
@@ -0,0 +1,213 @@
+#include <Arduino.h>
+#include <SPI.h>  // include the SPI library:
+
+#include "ads129xDriver.h"
+
+
+// The driver is configured to take control of an ADS1294 chip.
+// In file ads129xDriverConfig.h you have to specify the model of the ADS, bits per channel 
+// and the level of verbose of the library.
+
+// GPIO pins where CS, DRDY and RESET pins of the ADS chip are conected.
+/* ============== GPIO pins connected to ADS chip ===========*/
+// CS and DRDY pin of the ADS chip MUST be connected to Arduino
+
+const int IPIN_PWDN = 5;  
+const int PIN_CLKSEL = 9; 
+const int IPIN_RESET  =6;
+const int PIN_START = 7;//start
+const int IPIN_DRDY = 8;//data ready
+const int IPIN_CS = 10;
+const int DAISY_IN = 4;
+
+
+// Constructor
+ADS129xSensor adsSensor(IPIN_CS, IPIN_DRDY, IPIN_RESET);
+// ADS129xSensor adsSensor(ADS_CS_PIN, ADS_DRDY_PIN); // Only CS and DRDY pins are used
+// ADS129xSensor adsSensor(ADS_CS_PIN, ADS_DRDY_PIN, ADS_PIN_NOT_USED); // // Only CS and DRDY pins are used. Equivalent to the previous line
+
+// Helper function to print byte value in bits
+void printBits(byte myByte) {
+  for (byte mask = 0x80; mask; mask >>= 1) {
+    if (mask & myByte)
+      Serial.print('1');
+    else
+      Serial.print('0');
+  }
+}
+void arduinoSetup() ;
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Starting the ADS setup");
+
+  Serial.println("Calling ADS129xSensor.begin() method. Remember to call at the beginning");
+  // You MUST call begin() method before any other method of ADS129xSensor
+    arduinoSetup();
+    adsSensor.begin();
+
+  // It is not need. Only for example purposes
+  Serial.println("Example: reset value for all registers without reset command");
+  adsSensor.setAllRegisterToResetValuesWithoutResetCommand();
+
+  // Read ADS129x ID:
+  byte regValue = adsSensor.readRegister(ads::registers::id::REG_ADDR);
+
+  Serial.print("ID register: ");
+  printBits(adsSensor.readRegister(ads::registers::id::REG_ADDR));
+  Serial.println("");
+
+  Serial.print("Set sampling read to 1 kHz and low-power mode");
+  Serial.print("Keep in mind that when config1 or resp registers are changed, internal reset is performed. See the datasheet, section Reset");
+  // By default, ADS12xx is in low-power consumption and with a sample frequency of 250 Hz
+  adsSensor.writeRegister(ads::registers::config1::REG_ADDR, ads::registers::config1::LOW_POWR_16k_SPS);
+  Serial.print("The new value CONFIG1 register is ");
+  printBits(adsSensor.readRegister(ads::registers::config1::REG_ADDR));
+
+  // Setup of my circuit. In my case, it hadn't external reference,
+  Serial.println("Enabling internal reference buffer --> set PD_REFBUF to 1");
+  // If you change inividual bits with constants B_xx, you must add with the RESERVED_BITS constant value to be sure that you will
+  // write the right bits in the reserved bits in the register.
+  // Remember to write all desired configuration in a register  simultaneously. When you write a register, you delete all previous values
+  adsSensor.writeRegister(ads::registers::config3::REG_ADDR, ads::registers::config3::B_PD_REFBUF | ads::registers::config3::RESERVED_BITS);
+  
+  // Wait for internal reference to wake up. See page 15, section Electrical Characteristicsm in the datasheet,
+  delayMicroseconds(150);
+  
+  // Select test signal from chip
+  // As example, this 2 methods will keep the SPI open for ADS129x chip for faster configuration. The difference It's not noticeable for humans
+  // Be careful when you use this option. Read the documentation before using it.
+  adsSensor.writeRegister(ads::registers::config2::REG_ADDR, ads::registers::config2::TEST_SOURCE_INTERNAL, true);
+  // We will use the square signal at 4 Hz
+  adsSensor.writeRegister(ads::registers::config2::REG_ADDR, ads::registers::config2::TEST_FREQ_4HZ, true);
+
+  Serial.println("Starting channels configuration");
+  Serial.println("Channel 1: gain 1 and test signal as input");
+  adsSensor.enableChannelAndSetGain(1, ads::registers::chnSet::GAIN_6X, ads::registers::chnSet::ELECTRODE_INPUT);
+  Serial.println("Channel 2: gain 3 and test signal as input");
+  adsSensor.enableChannelAndSetGain(2, ads::registers::chnSet::GAIN_6X, ads::registers::chnSet::TEST_SIGNAL);
+  Serial.println("Channel 3: power-down and its inputs shorted (as Texas Instruments recommends)");
+  adsSensor.enableChannelAndSetGain(3, ads::registers::chnSet::GAIN_6X, ads::registers::chnSet::TEST_SIGNAL);
+
+  // adsSensor.disableChannel(3, true);
+  Serial.println("Channel 4 to max channels: set gain 6 and normal input");
+  for (uint8_t i = 4; i <= ADS_N_CHANNELS; i++)
+    adsSensor.enableChannelAndSetGain(i, ads::registers::chnSet::GAIN_6X, ads::registers::chnSet::ELECTRODE_INPUT);
+
+  Serial.println("Starting channels configuration");
+  adsSensor.sendSPICommandSTART();
+
+  // We need to put ADS in DATA or RDATC mode to receive new data
+  // Remember that in RDATAC mode, ADS ignores any SPI command sent if it is not SDATAC command 
+  Serial.println("Set ADS chip in read data (RDATA) mode");
+  adsSensor.sendSPICommandRDATA();  
+
+  // You can could the method end() to free GPIO used pins and resources. if you don't need any more de ADS
+  // You have to call begin() if you want to use again the ADS
+  // adsSensor.end()
+}
+
+void loop() {
+  // Serial.println("New loop iteration");
+
+  if (adsSensor.hasNewDataAvailable()) {
+    // Serial.println("New data available");
+    ads_data_t *adsData = adsSensor.getData();
+    //byte[3] statusWord = adsData->formatedData.statusWord;
+    //byte[3] channel1 = adsData->formatedData.channel[0];
+
+    // Serial.println("Status word (in binary):");
+    // printBits(adsData->formatedData.statusWord[0]);
+    // Serial.print("\t");
+    // printBits(adsData->formatedData.statusWord[1]);
+    // Serial.print("\t");
+    // printBits(adsData->formatedData.statusWord[2]);
+    // Serial.println("");
+
+    // Serial.println("Channel 1 sample:");
+    // Serial.print(adsData->formatedData.channel[0].hi);
+    // Serial.print("\t");
+    // Serial.print(adsData->formatedData.channel[0].mid);
+    // Serial.print("\t");
+    // Serial.print(adsData->formatedData.channel[0].low);
+    // Serial.print("\t");
+
+    // Transform sample to voltage
+    // Remember that is in MSB (most significant bit) order!!! Position 0
+    // ADS send samples in 24 bits and in binary twos complement format.
+    byte *byteSample = (byte*) &adsData->formatedData.channel[0];
+    // For an easy manipulation, transform twos complement format to binary offset. It is equivalent to an unsigned value
+    // with V_Ref offset. The new zero is equivalent in the real world to maximum negative value possible
+    byte mask = byteSample[0] & 0x80;
+    mask ^= 0x80;
+    byte offsetBinary = mask | (byteSample[0] & 0x7F);
+    // Sample in binary offset of 24 bits. Remember, it has in MSB format. We can access with adsData->formatedData.channel[0].{hi,mid,low} fields
+    int32_t sampleValue = (offsetBinary << 16) + (byteSample[1] << 8) + byteSample[2];
+
+    Serial.println(sampleValue, DEC);
+    // Remove offset and scale the variable before printing in the monitor
+    float v_ref = 2.4; // V_ref for the ADS1294. In my setup, VREFP = 2.4V (see VREF_4V bit in config3) and VREFN = 0V (connected to ground)
+    float channelGain = 1;
+    float sampleInVoltsAndWithOffset = v_ref * ((float) sampleValue) / (pow(2, 23) - 1) / channelGain;
+    float sampleInVolts = sampleInVoltsAndWithOffset - v_ref;
+    // Serial.print("Equivalent value in milivolts");
+    // Serial.print("\t");
+    // Serial.println(sampleInVolts * 1e3, 5);
+
+    /*
+        // Print in binary
+        Serial.print("Original bits for channel 1: ");
+        printBits(adsData->formatedData.channel[0].hi);
+        printBits(adsData->formatedData.channel[0].mid);
+        printBits(adsData->formatedData.channel[0].low);
+        Serial.println("");
+        Serial.print("Bits in offset binary for channel 1: ");
+        Serial.println(sampleValue, BIN);
+    */
+
+    // Read new data. We aren't in RDATAC mode
+    adsSensor.sendSPICommandRDATA();
+  } 
+  // else {
+  //   Serial.println("No new data");
+  // }
+
+  // Serial.println("");
+  
+  // Serial.println("Wait 1 second before execute again the loop");
+  // delay(100);
+}
+
+
+
+
+
+void arduinoSetup() {
+  pinMode(IPIN_CS, OUTPUT);
+  pinMode(PIN_START, OUTPUT);
+  pinMode(IPIN_DRDY, INPUT);
+  pinMode(PIN_CLKSEL, OUTPUT);//*optional
+  pinMode(IPIN_RESET, OUTPUT);//*optional
+  pinMode(IPIN_PWDN, OUTPUT);//*optional
+  //start small peripheral interface
+  // SPI.begin();
+  // SPI.setBitOrder(MSBFIRST);
+  // #ifndef isDUE
+  // SPI.setClockDivider(SPI_CLOCK_DIV4); //http://forum.pjrc.com/threads/1156-Teensy-3-SPI-Basic-Clock-Questions
+  // #endif
+  // SPI.setDataMode(SPI_MODE1);
+  // Serial.println("SPI setup OK"); 
+
+  //Start ADS1298
+  delay(500); //wait for the ads129n to be ready - it can take a while to charge caps
+  digitalWrite(PIN_CLKSEL, HIGH);//*optional
+  delay(1);digitalWrite(IPIN_PWDN, HIGH);//*optional - turn off power down mode
+  digitalWrite(IPIN_RESET, HIGH);delay(100);//*optional
+  digitalWrite(IPIN_RESET, LOW);delay(1);//*optional
+  digitalWrite(IPIN_RESET, HIGH);delay(1);  //*optional Wait for 18 tCLKs AKA 9 microseconds, we use 1 millisec
+  // Send SDATAC Command (Stop Read Data Continuously mode)
+  delay(100); //pause to provide ads129n enough time to boot up...
+
+  Serial.println("ADS1294 setup OK"); 
+} 
